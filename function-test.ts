d9bbbe245832477d9e1ac5341d857785c4974ea3
chewzzz <chewziqing@gmail.com>
2022-12-17 20:13:09 +0800

    Add function with diff params and return type

--- function-test.ts
+++ function-test.ts
@@ -1,3 +1,5 @@
-import addNumbers from "./function";
+import addNumbers, { addStrings } from "./function";
 
 console.log(addNumbers(2, 5))
+console.log(addStrings('Hello', 'World'))
+console.log(addStrings('Hello'))
--- function.ts
+++ function.ts
@@ -6,4 +6,27 @@ function addNumbers(a: number, b: number): number {
 // ts dont have module.exports
 // module.exports = addNumbers;
 
-export default addNumbers 
+export default addNumbers
+export const addStrings = (s1: string, s2: string = 'Empty'): string => {
+    return `${s1} ${s2}`
+}
+
+// union types
+export const format = (title: string, param: string | number): string => {
+    return `${title} ${param}`
+}
+
+// void function
+export const printFormat = (title: string, param: string | number) => {
+    console.log(format(title, param))
+}
+
+// return promise
+export const fetcgData = (url: string): Promise<string> => {
+    return Promise.resolve(`Data from ${url}`)
+}
+
+// rest parameter
+function introduce(salutation: string, ...names: string[]): string {
+    return `${salutation} ${names.join(" ")}`
+}
