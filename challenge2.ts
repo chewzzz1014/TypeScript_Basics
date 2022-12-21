0d0206bf520cb0847155877ee3afb512f489058f
chewzzz <chewziqing@gmail.com>
2022-12-21 15:28:21 +0800

    Add challenge2.ts

---
+++ challenge2.ts
@@ -0,0 +1,14 @@
+function myForEach<T>(items: T[], forEachFunc: (v: T) => void) {
+    items.reduce((a, v) => {
+        forEachFunc(v)
+        return undefined
+    }, undefined)
+}
+
+myForEach(['a', 'b', 'c'], (v) => console.log(`forEach ${v}`))
+
+function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
+    return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [])
+}
+
+myFilter([1, 2, 3, 4, 5, 6, 7, 8], (v) => v % 2 === 0)
