a845d65a20939089e4032010a7f2b00202b8904c
chewzzz <chewziqing@gmail.com>
2022-12-21 15:00:38 +0800

    Add generic interface

--- generics.ts
+++ generics.ts
@@ -11,8 +11,41 @@ function simpleReactState<T>(initial: T): [() => T, (v: T) => void] {
 }
 
 
-// generic can be any type
+// inferred generics
 const [numGetter, numSetter] = simpleReactState(3)
 const [stateGetter, stateSetter] = simpleReactState('yoyoyyo')
+
+// overide onferred generics
+const [state1Getter, state1Setter] = simpleReactState<string | null>(null)
+console.log(`State 1: ${state1Getter()}`)
+// only accepys null
+state1Setter('hello')
+console.log(`State 1: ${state1Getter()}`)
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+interface Rank<RankItem> {
+    item: RankItem,
+    rank: number
+}
+
+function ranker<RankItem>(
+    items: RankItem[],
+    rank: (v: RankItem) => number
+): RankItem[] {
+    const ranks: Rank<RankItem>[] = items.map(item => {
+        return {
+            item,
+            rank: rank(item)
+        }
+    })
+
+    // sort ascendingly
+    ranks.sort((a, b) => a.rank - b.rank)
+
+    // return item of ranls only
+    return ranks.map((rank) => rank.item)
+}
 
 
