8286c48723e523f0e656e7d0594ebdcc7bc70cca
chewzzz <chewziqing@gmail.com>
2022-12-26 17:24:00 +0800

    Add myMap function and fixed type error in return type

--- challenge2.ts
+++ challenge2.ts
@@ -1,4 +1,4 @@
-function myForEach<T>(items: T[], forEachFunc: (v: T) => void) {
+function myForEach<T>(items: T[], forEachFunc: (v: T) => void): void {
     items.reduce((a, v) => {
         forEachFunc(v)
         return undefined
@@ -7,8 +7,20 @@ function myForEach<T>(items: T[], forEachFunc: (v: T) => void) {
 
 myForEach(['a', 'b', 'c'], (v) => console.log(`forEach ${v}`))
 
+/////////////////////////////////////////////////////////////
+
 function myFilter<T>(items: T[], filterFunc: (v: T) => boolean): T[] {
-    return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [])
+    return items.reduce((a, v) => (filterFunc(v) ? [...a, v] : a), [] as T[])
 }
 
-myFilter([1, 2, 3, 4, 5, 6, 7, 8], (v) => v % 2 === 0)
+console.log(myFilter([1, 2, 3, 4, 5, 6, 7, 8], (v) => v % 2 === 0))
+
+
+/////////////////////////////////////////////////////////////
+
+function myMap<T, K>(items: T[], mapFunc: (v: T) => K): K[] {
+    return items.reduce((a, v) => [...a, mapFunc(v)], [] as K[])
+}
+
+console.log(myMap([1, 2, 3, 4, 5, 6, 7, 8], (v) => (v * 10).toString()))
+
