536803e6d8c93411267e66b44040f0177eaad850
chewzzz <chewziqing@gmail.com>
2023-01-04 18:38:45 +0800

    Add child class

--- class/database.ts
+++ class/database.ts
@@ -3,9 +3,14 @@ interface Database {
     set(id: string, value: string): void
 }
 
+interface Persistable {
+    saveToString(): string;
+    restoreFromString(storedState: string): void;
+}
+
 // class
 class InMemoryDatabase implements Database {
-    private db: Record<string, string> = {};
+    protected db: Record<string, string> = {};
     get(id: string): string {
         return this.db[id]
     };
@@ -14,10 +19,27 @@ class InMemoryDatabase implements Database {
     }
 }
 
-const myDB = new InMemoryDatabase()
-myDB.set('foo', 'bar')
+class PersistentMemoryDB extends InMemoryDatabase implements Persistable {
+    saveToString(): string {
+        return JSON.stringify(this.db)
+    };
+    restoreFromString(storedState: string): void {
+        this.db = JSON.parse(storedState)
+    }
+}
 
+const myDB = new PersistentMemoryDB()
+myDB.set('foo', 'bar')
 // error: db is private
 //myDB.db['foo'] = 'baz'
 
 console.log(myDB.get('foo'))
+const saved = myDB.saveToString()
+// updte myDB.db after save its state
+myDB.set('foo', 'db - bar')
+
+// myDB2.db = {"foo": "bar"}
+const myDB2 = new PersistentMemoryDB()
+myDB2.restoreFromString(saved)
+console.log(myDB2.get('foo'))
+
