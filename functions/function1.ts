8227d67391a22b51eb34b538d353463be3b013b1
chewzzz <chewziqing@gmail.com>
2023-01-14 16:50:25 +0800

    Add call signature and constructor signature of function

--- functions/function1.ts
+++ functions/function1.ts
@@ -14,5 +14,29 @@ greeter(printToConsole)
 // function as type 
 type GreetFunction = (s: string) => void
 function greeterType(fn: GreetFunction) {
+    // function declaration here 
+}
 
+//////////////////////////////////////////////////////////
+
+// function with callable properties
+type DescribableFunction = {
+    desc: string, // properties of function
+    (someArg: number): boolean, // function params and return type
+}
+
+function doSmth(fn: DescribableFunction) {
+    console.log(`${fn.desc} returned ${fn(6)}`)
+}
+
+//////////////////////////////////////////////////////////
+
+// function with constructor
+type SomeConstructor = {
+    new(s: string): string;    // constructor signature
+    ()
+}
+
+function fn(ctor: SomeConstructor) {
+    return new ctor('Hello')
 }
