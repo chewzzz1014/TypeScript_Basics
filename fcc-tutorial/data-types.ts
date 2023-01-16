016f56ad997139bc73e7746b70904cce96dcb32a
chewzzz <chewziqing@gmail.com>
2023-01-16 22:45:32 +0800

    Add data-types.ts

---
+++ fcc-tutorial/data-types.ts
@@ -0,0 +1,8 @@
+// string, number and boolean
+let myName: string = 'chewzzz'
+let myNameUpper = myName.toUpperCase() // can apply string method on it
+console.log(myNameUpper)
+
+let myAge: number = 20
+let isFemale: boolean = true
+
