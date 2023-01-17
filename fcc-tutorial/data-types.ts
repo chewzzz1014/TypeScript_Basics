4cfda7bf318ba1ead375ee9a01bf7c9ad940f1a6
chewzzz <chewziqing@gmail.com>
2023-01-17 16:35:28 +0800

    Print the result

--- fcc-tutorial/data-types.ts
+++ fcc-tutorial/data-types.ts
@@ -14,4 +14,9 @@ let numList: number[] = [2, 4, 6, 8]
 let greaterThan3 = numList.filter(n => n > 3) // number[]
 let find2 = numList.find(n => n === 2) // number | undefined. the number might not found
 let mapNumList = numList.map(n => `${n}`) // string[]
+let sum = numList.reduce((acc, ele) => acc + ele, 0)
+
 console.log(greaterThan3)
+console.log(find2)
+console.log(mapNumList)
+console.log(sum)
