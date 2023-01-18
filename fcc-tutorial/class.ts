5b256f89ccde30fb7a3562682bdb8e3448c5830a
chewzzz <chewziqing@gmail.com>
2023-01-18 13:52:18 +0800

    Add static method

--- fcc-tutorial/class.ts
+++ fcc-tutorial/class.ts
@@ -3,7 +3,7 @@
 class Employee {
     #id: number; // private
 
-    name: string
+    protected name: string // protected: only available within the class and child class
 
     address: string
 
@@ -13,15 +13,33 @@ class Employee {
         this.#id = id
     }
 
+    // static method
+    static getEmployeeCount(): number {
+        return 50
+    }
+
     getNameWithAddress(): string {
-        return `${this.name} ${this.address}`
+        return `${this.name} is staff at ${this.address}`
+    }
+}
+
+class Manager extends Employee {
+    constructor(id: number, name: string, address: string) {
+        super(id, name, address)
+    }
+
+    getNameWithAddress(): string {
+        return `${this.name} is manager at ${this.address}`
     }
 }
 
 let emp = new Employee(3, 'chewzzz', 'Malaysia')
 
+console.log(`There are ${Employee.getEmployeeCount()} employees\n`)
 console.log(emp)
 console.log(emp.getNameWithAddress())
 // error! id is private
 // console.log(emp.id)
 
+let myManager = new Manager(5, 'wwwww', 'Malaysia')
+console.log(myManager.getNameWithAddress())
