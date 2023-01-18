1c95bb6db1a09c9fa9d5db991e8fa65aff8d3b20
chewzzz <chewziqing@gmail.com>
2023-01-18 13:14:15 +0800

    Add class.ts

---
+++ fcc-tutorial/class.ts
@@ -0,0 +1,17 @@
+// class
+
+class Employee {
+    id!: number;
+
+    name!: string
+
+    address!: string
+}
+
+let emp = new Employee()
+emp.id = 3
+emp.name = 'chewzzz'
+emp.address = 'Highway 71'
+
+console.log(emp)
+
