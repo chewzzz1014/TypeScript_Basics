b24a315869efa7ccb63c1d868440f4dc8c7243ba
chewzzz <chewziqing@gmail.com>
2023-01-20 20:39:59 +0800

    Add array destructing

--- fcc-tutorial/interface.ts
+++ fcc-tutorial/interface.ts
@@ -33,3 +33,16 @@ let user: User = {
 }
 
 printAddressInfo(user.address)
+
+const add: Address = {
+    street: 'Tailwind Road',
+    city: 'Mertajam',
+    state: 'Penang'
+}
+
+// array destructing
+let [user1, user2, user3]: User[] = [
+    { name: 'chewzzz', age: 20, id: 1, email: 'z@email.com', address: add },
+    { name: 'xxxxxxx', age: 30, id: 2, email: 'x@email.com', address: add },
+    { name: 'yyyyyy', age: 40, id: 3, email: 'y@email.com', address: add },
+]
