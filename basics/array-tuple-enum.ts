5cc9e27d560715085a97bf8cc0f08540b1bd5d47
chewzzz <chewziqing@gmail.com>
2023-02-16 13:40:50 +0800

    Add array-tuple-enum.ts

---
+++ basics/array-tuple-enum.ts
@@ -0,0 +1,26 @@
+// array
+let myStringArr: string[] = ['one', 'two', 'three']
+let myUnionArr: (number | string)[] = ['one', 'two', 3] // ts inferred as (number|string)[] type
+let myUnionArr1 = ['one', 2, false] // ts inferred as (string | number | boolean)[] type
+
+
+console.log(convertIt(['12', '233', '222']))
+// convert string array to number array
+function convertIt(a: string[]): number[] {
+    return a.map((ele: string) => Number(ele))
+}
+
+
+// tuple: order of the types matter
+let myTuple: [string, number, boolean] = ['one', 1, true]
+// error
+// myTuple[0] = false 
+
+
+// object
+
+
+let student = {
+    name: 'chewzzz',
+    matric: 22222
+}
