92a40e2e3ca8b3c7c55fdf5fcfb9aba3c23b677f
chewzzz <chewziqing@gmail.com>
2023-02-17 17:46:48 +0800

    Add type alias for function

---
+++ functions/basic.ts
@@ -0,0 +1,13 @@
+// function
+
+type MyFunc = (a: number, b: number) => number
+
+const addNum: MyFunc = (a, b) => a + b
+
+const subNum: MyFunc = (a, b) => a - b
+
+const mulNum: MyFunc = (a, b) => a * b
+
+const divNum: MyFunc = (a, b) => a / b
+
+const powOf: MyFunc = (a, b) => a ** b
