1c1b4a9012b22dbc9bb0b3cdfe463c9898d8c361
chewzzz <chewziqing@gmail.com>
2023-02-17 17:59:06 +0800

    optional params for function

--- functions/basic.ts
+++ functions/basic.ts
@@ -15,3 +15,15 @@ const mulNum: MyFunc = (a, b) => a * b
 const divNum: MyFunc = (a, b) => a / b
 
 const powOf: MyFunc = (a, b) => a ** b
+
+
+// narrowing VS optional params
+const addAll = (a: number, b: number, c: number): number => {
+    if (c === 0 || c)
+        return a + b + c
+    return a + b
+}
+
+const sumAll = (a: number, b: number, c: number = 0): number => {
+    return a + b + c
+}
