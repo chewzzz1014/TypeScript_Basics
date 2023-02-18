2a0c9c9d64dc1a870da378cdff3046bab9469949
chewzzz <chewziqing@gmail.com>
2023-02-18 14:29:30 +0800

    use <> for type assestion

--- types/type-assertion.ts
+++ types/type-assertion.ts
@@ -3,6 +3,12 @@ type Two = string | number
 type Three = 'hello' // literal type
 
 // convert to more or less specific type
+
+// 1. use `as` alias
 let varA: One = 'hello'
 let varB = varA as 'hello' // varB has type 'hello' (literal type). To less specific
 let varC = varA as Three // to more specific
+
+// 2. use <Type> (can'ts be used in TSX)
+let varD = <One>'world'
+let varE = <string | number>'world'
