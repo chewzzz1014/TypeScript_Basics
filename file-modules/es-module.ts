dc0eaf19aa872742c73887212b711d8e057ea8ce
chewzzz <chewziqing@gmail.com>
2023-03-12 19:04:55 +0800

    Add es module export

---
+++ file-modules/es-module.ts
@@ -0,0 +1,17 @@
+// ES Module syntax
+
+// prefix 'export'
+export let myName = 'chewzzz'
+export type SomeType = {
+    foo: string
+}
+
+// dedicated export stmt
+let myName1 = 'chewzzz'
+type SomeType1 = {
+    foo: string
+}
+export {
+    myName1,
+    SomeType1
+}
