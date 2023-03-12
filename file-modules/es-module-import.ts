61aaa05968a66d84cec7be1cf2f695eed59a209c
chewzzz <chewziqing@gmail.com>
2023-03-12 19:11:25 +0800

    add es module impor

--- file-modules/es-module-import.ts
+++ file-modules/es-module-import.ts
@@ -1,1 +1,14 @@
 // ES Module syntax - Import
+
+// import variable
+import {
+    myName, SomeType
+} from "./es-module-export";
+
+
+// import with renaming
+import { myName1 as nameV2 } from "./es-module-export";
+
+
+// import everything into a name
+import * as myInfo from './es-module-export'
