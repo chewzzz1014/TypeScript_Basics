4a4ae7597e6221b3a0df4cffda63f2e22a7e62f2
chewzzz <chewziqing@gmail.com>
2023-03-14 14:40:51 +0800

    Add Coder class

---
+++ class/basics-1.ts
@@ -0,0 +1,18 @@
+class Coder {
+    name: string
+    music: string
+    age: number
+    lang: string
+
+    constructor(
+        public name: string,
+        music: string,
+        age: number,
+        lang: string
+    ) {
+        this.name = name
+        this.music = music
+        this.age = age
+        this.lang = lang
+    }
+}
