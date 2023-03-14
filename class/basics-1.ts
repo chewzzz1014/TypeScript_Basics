9de7cc8f9c02052be2d44e1f52bdfc79589104c2
chewzzz <chewziqing@gmail.com>
2023-03-14 14:57:44 +0800

    Add class method; create class instance

--- class/basics-1.ts
+++ class/basics-1.ts
@@ -8,11 +8,18 @@ class Coder {
         public readonly name: string,
         public music: string,
         private age: number,
-        protected lang: string
+        protected lang: string = 'TypeScript' // default value
     ) {
         this.name = name
         this.music = music
         this.age = age
         this.lang = lang
     }
+
+    public getAge() {
+        return `Hello, I'm ${this.age} years old.`
+    }
 }
+
+const me = new Coder('chewzzz', 'JPOP', 21, 'TypeScript')
+console.log(me.getAge())
