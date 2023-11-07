05638ea43a4f67d12196aa49e93747ea0ea8eef0
chewzzz <chewziqing@gmail.com>
2023-11-07 21:20:11 +0800

    tuple, enum

--- ts-tutorial/ts-types/index.ts
+++ ts-tutorial/ts-types/index.ts
@@ -21,3 +21,61 @@ let items = [0, 1, null, 'Hi']; // inferred type is (number|string)[]
 document.addEventListener('click', function (event) {
     console.log(event.button); // MouseEvent.button
 });
+
+
+
+// tuple
+// order of type is important
+let skill: [string, number]
+skill = ['Programming', 5]
+
+// optional element in tuple
+let bgColor, headerColor: [number, number, number, number?]
+bgColor = [0, 255, 255, 0.5]
+headerColor = [0, 255, 255]
+
+
+// enum
+enum Month {
+    Jan,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+}
+function isItSummer(month: Month) {
+    let isSummer: boolean
+    switch(month) {
+        case Month.Jun:
+        case Month.Jul:
+        case Month.Aug:
+            isSummer = true
+            break
+        default:
+            isSummer = false
+            break
+    }
+    return isSummer
+}
+
+enum ApprovalStatus {
+    draft,
+    submitted,
+    approved,
+    rejected
+}
+const request = {
+    id: 1,
+    status: ApprovalStatus.approved,
+    description: 'Please approve this request'
+}
+if (request.status === ApprovalStatus.approved) {
+    console.log('Send email to the Applicant')
+}
