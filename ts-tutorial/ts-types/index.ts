bb124860813c2b112cfb6384349323410e3e4f13
chewzzz <chewziqing@gmail.com>
2023-11-07 20:37:12 +0800

    initial commit

---
+++ ts-tutorial/ts-types/index.ts
@@ -0,0 +1,17 @@
+let myName1: string = 'chewzzz'
+let myAge1: number = 21
+let isStudent: boolean = true
+let gpa: number[] = [3.88, 3.90, 3.79]
+
+let person: {
+    name: string,
+    age: number
+}
+person = {
+    name: 'chewzzz',
+    age: 21
+}
+
+function greeting(name: string): string {
+    return `Hallo ${name}!`
+}
