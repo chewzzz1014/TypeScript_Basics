e86f015ba754ddada7df7f7bbe8277b0ae8d08d2
chewzzz <chewziqing@gmail.com>
2023-12-19 16:51:38 +0800

    readonly fields

--- ts-tutorial/objects.ts
+++ ts-tutorial/objects.ts
@@ -14,3 +14,27 @@ class Counter {
         return ++this.current;
     }
 }
+
+class Person {
+    _ssn: string;
+    _firstName: string;
+    _lastName: string;
+    _age: number;
+    readonly _birthDate: Date;
+
+    constructor(ssn: string, firstName: string, lastName: string, birthDate: Date, age: number) {
+        this._ssn = ssn;
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._birthDate = birthDate;
+        this._age = age
+    }
+
+    public get age() {
+        return this._age
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
