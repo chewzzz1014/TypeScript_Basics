8dd11f02f503a4867dc03e799ff64813094ff5ee
chewzzz <chewziqing@gmail.com>
2023-12-19 13:48:57 +0800

    class declaration

---
+++ ts-tutorial/objects.ts
@@ -0,0 +1,16 @@
+class Counter {
+    private current: number = 0;
+    count(): number;
+    count(target: number): number[];
+    count(target?: number): number | number[] {
+        if (target) {
+            let values = [];
+            for (let start = this.current; start <= target; start++) {
+                values.push(start);
+            }
+            this.current = target;
+            return values;
+        }
+        return ++this.current;
+    }
+}
